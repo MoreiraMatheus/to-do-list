fbff1559e62a4fab9b61115ebb542679feb4c86c
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-07-10 15:46:12 -0300

    refazendo script

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,69 @@
-const addTarefa = document.querySelectorAll('button')[0]
+const btAddTarefa = document.querySelectorAll('button')[0]
 const listaTarefas = document.getElementById('lista-tarefas')
-const deleteTarefa = document.querySelectorAll('button.delete')
-const editTarefa = document.querySelectorAll('button.edit')
+let btDelete = listaTarefas.querySelectorAll('button.delete')
+let btEdit = listaTarefas.querySelectorAll('button.edit')
 
-addTarefa.addEventListener('click', () => {
-    const adicionarNovaTarefa = document.querySelectorAll('input')[0]
-    let descreveTarefa = adicionarNovaTarefa.value
-    if(descreveTarefa == ''){
-        window.alert('descreva a tarefa para adicionar ela')
-    }
-    else{
-        let textoTarefa = 
-        `<div class="tarefa">
-            <p>${descreveTarefa}</p>
-            <div class="bts">
-                <input type="checkbox" name="" id="check">
-                <button><img src="src/img/trash.png" alt=""></button>
-                <button><img src="src/img/write.png" alt=""></button>
-            </div>
-        </div>`
-        adicionarNovaTarefa.value = ''
-        listaTarefas.innerHTML += textoTarefa
-    }
-    console.log(listaTarefas.querySelectorAll('div.tarefa'))
+// atualiza dados importantes
+function refresh(){
+    btDelete = listaTarefas.querySelectorAll('button.delete')
+    btEdit = listaTarefas.querySelectorAll('button.edit')
+}
+
+btAddTarefa.addEventListener('click', () => {
+    //criar div
+    const inputTarefa = document.querySelector('input')
+    const novaDiv = criarDiv(inputTarefa.value)
+    inputTarefa.value = ''
+    //adicionar div a lista de tarefas
+    listaTarefas.appendChild(novaDiv)
+    //dar refresh
+    refresh()
 })
+
+function criarDiv(nomeTarefa){
+    //criar a div de class tarefa
+    const tarefaNova = document.createElement('div')
+    tarefaNova.classList.add('tarefa')
+
+    //parágrafo novo
+    const pNovo = document.createElement('p')
+    pNovo.innerText = nomeTarefa
+    //div onde ficará os novos botões
+    const btsNovos = document.createElement('div')
+    btsNovos.classList.add('bts')
+    
+    tarefaNova.appendChild(pNovo)
+    tarefaNova.appendChild(btsNovos)
+
+    //adiciona os botões
+    //checkbox
+    const checkboxNova = document.createElement('input')
+    checkboxNova.setAttribute('type', 'checkbox')
+    
+    //delete tarefa
+    const deleteBtNovo = document.createElement('button')
+    deleteBtNovo.classList.add('delete')
+    const imgDeleteBt = document.createElement('img')
+    imgDeleteBt.setAttribute('src', 'src/img/trash.png')
+    deleteBtNovo.appendChild(imgDeleteBt)
+    //edit tarefa
+    const editBtNovo = document.createElement('button')
+    editBtNovo.classList.add('edit')
+    const imgEditBt = document.createElement('img')
+    imgEditBt.setAttribute('src', 'src/img/write.png')
+    editBtNovo.appendChild(imgEditBt)
+    
+    btsNovos.appendChild(checkboxNova)
+    btsNovos.appendChild(deleteBtNovo)
+    btsNovos.appendChild(editBtNovo)
+
+    return tarefaNova
+}
+
+//Criar funções dos botões das tarefas
+
+// Checkbox
+
+// Delete
+
+// Edit
